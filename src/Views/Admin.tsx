e95833a19cab552eac6d567f3c77ac079c4c5a77
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2024-01-25 02:04:18 -0500

    Create QR code works

--- src/Views/Admin.tsx
+++ src/Views/Admin.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import './Styles/Admin.scss'
 import createFirestoreLocation from "../Helpers/CreateLocation";
 import { coordinatesToURLEncryptedString } from "../Helpers/CoordsEncrypt";
+import QRCode from "react-qr-code";
 
 function Admin() {
     const [ latitude, setLatitude ]     = useState("");
@@ -25,16 +26,29 @@ function Admin() {
         };
 
         setURL(hostname + locationData.id);
-        createFirestoreLocation(locationData);
+        //createFirestoreLocation(locationData);
     };
 
     return (
         <div id="Admin">
-            <h1>Hello, Guide.</h1>
+            <header>
+                <h1>Hello, Guide.</h1>
+            </header>
             <div id="LocationCreator">
+                { url &&
+                    <div id="QRCode">
+                        <QRCode
+                            size={500}
+                            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                            value={url}
+                            viewBox={`0 0 500 500`}
+                        />
+                    </div>
+                }
+                <p> { url } </p>
+
                 <input placeholder="Latitude" value={latitude} onChange={handleLatitudeChange} type="text" />
                 <input placeholder="Longitude" value={longitude} onChange={handleLongitudeChange} type="text" />
-                <p> {  } </p>
                 <button onClick={handleLocationCreate}>Create Location</button>
             </div>
         </div>
